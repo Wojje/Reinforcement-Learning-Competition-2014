68d421c075da89102e9540ad2aaea80e36fca46e
Johan Andersson <horv@chalmers.it>
2014-03-30 18:48:25 +0200

    Updated the interface in order to take action into account for probability transition function

--- DDV/src/mdp/MDP.java
+++ DDV/src/mdp/MDP.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import utils.ActionStep;
 import utils.State;
+import utils.StateActionState;
 
 public interface MDP {
 	
+	public State getStartingState();
 	public List<State> getStates();
 	public List<ActionStep> getActions();
-	public double probTransition(State s, State sprime);
+	public double probTransition(StateActionState sas);
 	public double reward(State s, State sprime);
+	
 	
 	
 
